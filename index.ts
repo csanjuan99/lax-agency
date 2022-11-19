f54576e57fd366e2fd30ed8b39e2313ed788207d
Carlos Daniel Sanjuan <carlosdsanjuan@outlook.com>
2022-11-18 21:53:48 -0500

    Functionality

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
 import express, {Express, Request, Response} from 'express';
 import {routerApi} from './routes';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 
+const options: cors.CorsOptions = {
+    origin: '*',
+};
+
+app.use(cors(options));
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
